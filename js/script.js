f7222f6714b6fc699c42808c466e11ac311179a7
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-22 14:50:51 +0100

    get variables from DOM

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const euroKm = 0.2762;
+
+const kmToGo = document.querySelector("input#km").value;
+
+console.log("km", kmToGo);
+
+const yourAge = document.querySelector("input#age").value;
+
+console.log("age", yourAge);
