7154df38bf5770d9c1c9a4a3edbf8d75ad5d6ec8
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-22 15:30:11 +0100

    discounti for minor and over 65

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 const euroKm = 0.2762;
 
-const kmToGo = document.querySelector("input#km").value;
-
+const kmToGo = parseInt(document.querySelector("input#km").value);
+// kmToGo = parseInt(kmToGo)
 console.log("km", kmToGo);
 
-const yourAge = document.querySelector("input#age").value;
-
+const yourAge = parseInt(document.querySelector("input#age").value);
 console.log("age", yourAge);
+
+let ticketCost = euroKm * kmToGo;
+console.log("prezzo intero", ticketCost);
+
+const buttonCalculator = document.querySelector("button");
+
+buttonCalculator.addEventListener("click", function(){
+  if(yourAge < 18){
+    ticketCost *= 0.825;
+    console.log("Prezzo per minorenni", ticketCost);
+  } else if (yourAge > 65){
+    ticketCost *= 0.667;
+    console.log("Prezzo per over 65", ticketCost);
+  }
+
+  document.querySelector("p").innerHTML= `Il tuo biglietto costa ${ticketCost.toFixed(2)}`;
+})
